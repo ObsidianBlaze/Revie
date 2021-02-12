7ab004e02b4d686aed97e962bb9e9f7f3048859c
ObsidianBlaze <wijojames@gmail.com>
2021-02-12 23:46:08 +0100

    Setting up the models and migrations

---
+++ database/migrations/2021_02_12_223821_create_reviews_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('apartment_id')->unsigned();
+            $table->bigInteger('helpful')->default(0);
+            $table->string('video')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('apartment_id')->references('id')->on('apartments')->onDelete('cascade')->onUpdate('cascade');
+
+        });
+    }
+//User_Id foreign key
+//Apartment_Id foreign key
+//helpful default 0
+//Videos(nullable)
+//Images(nullable)
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
